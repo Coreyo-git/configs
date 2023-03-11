15c864e84127e9390971ce97645346150454a134
coreyo-git <corey.oconnor@live.com.au>
2023-03-11 14:39:21 +1000

    fixed screenSaver picture path

--- screenSaverShuffle.sh
+++ screenSaverShuffle.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 # Searches for all files in folder, randomizes and then chooses the head/top file
 # Mounts image with feh
-randImg=`find /home/coreyo/Documents/Pictures/DanMachi/ -type f | sort -R | head -1`
+randImg=`find /home/coreyo/Documents/Pictures/ -type f | sort -R | head -1`
 feh --bg-scale $randImg
 
 # Crontab that runs the script every minute and applys it to the display
